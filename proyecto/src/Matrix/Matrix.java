3c050a0bc767f256b75b6b699ac5d16b06322712
Yael <yael.rodriguez@iteso.mx>
2023-05-02 18:38:23 -0600

    Matrix empezada

--- proyecto/src/Matrix/Matrix.java
+++ proyecto/src/Matrix/Matrix.java
@@ -1,42 +1,59 @@
 package Matrix;
 
 import Expressions.Expression;
+import Expressions.Real;
 
 public class Matrix{
 
-    private static Expression[][] matrix;
-    private static int rows;
-    private static int cols;
+    private Expression[][] matrix;
+    private final int rows;
+    private final int cols;
 
     public static int MIN_SIZE = 1;
     protected static int DEFAULT_SIZE = 3;
 
-    public Matrix(){}
-
-    public Matrix(int size){}
-
-    public Matrix(int rows, int cols){}
-
-    public static int getRows() {
-        return rows;
+    //CONSTRUCTORES
+    public Matrix(){
+        this(DEFAULT_SIZE);
     }
 
-    public static int getCols() {
-        return cols;
+    public Matrix(int size){
+        this(size, size);
     }
 
-    public int get(int rows, int cols){
-        return 0;
-    };
+    public Matrix(int rows, int cols){
+        this.rows = Math.max(MIN_SIZE, rows);
+        this.cols = Math.max(MIN_SIZE, cols);
+        this.matrix = new Expression[this.rows][this.cols];
+
+    }
+
+    // GETTERS AND SETTERS
+    public int getRows() {
+        return this.rows;
+    }
+
+    public int getCols() {
+        return this.cols;
+    }
+
+    public Expression get(int rows, int cols){
+        if(rows < 0 || rows >= this.rows || cols < 0 || cols >= this.cols) return null;
+        return this.matrix[rows][cols];
+    }
 
     public int set(int rows, int cols, Expression number){
+        if(rows < 0 || rows >= this.rows || cols < 0 || cols >= this.cols) return 1;
+        this.matrix[rows][cols] = number;
         return 0;
     }
 
     public int set(int rows, int cols, double number){
-        return 0;
+        return set(rows, cols, new Real(number));
     }
 
+
+    // QUE HUEVA, LUEGO VEO
     @Override
     public String toString() {
         return super.toString();
@@ -46,7 +63,6 @@ public class Matrix{
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
