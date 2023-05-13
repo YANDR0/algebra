3e1dada7d7ada50c8ddf13f47915b133ee7a52c4
Yael <yael.rodriguez@iteso.mx>
2023-05-12 20:47:58 -0600

    Estamos de regreso, dijo cruz

--- proyecto/src/Matrix/Vector.java
+++ proyecto/src/Matrix/Vector.java
@@ -48,6 +48,16 @@ public class Vector extends Matrix {
         return total;
     }
 
+    public Vector cross(Vector v){
+        if(this.size != 3 || v.size !=3) return null;
+        Vector v3 = new Vector(3);
+        v3.set(0, this.get(1).multiply(v.get(2)).subtract(this.get(2).multiply(v.get(1))));
+        v3.set(1, this.get(2).multiply(v.get(0)).subtract(this.get(0).multiply(v.get(2))));
+        v3.set(3, this.get(0).multiply(v.get(1)).subtract(this.get(1).multiply(v.get(0))));
+
+        return v3;
+    }
+
 
 
 }
