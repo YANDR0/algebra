7e727698d591492d077b34cee8aea7e27fc9384d
Yael <yael.rodriguez@iteso.mx>
2023-05-14 12:19:22 -0600

    BBBBBB

--- proyecto/src/Expressions/Real.java
+++ proyecto/src/Expressions/Real.java
@@ -2,17 +2,33 @@ package Expressions;
 
 public class Real extends Imaginary{
 
+    /**
+     * Crea un número real equivalente a cero
+     */
     public Real(){
     }
 
+    /**
+     * Crea un número real con su parte real igual a lo dado
+     * @param n Valor del número
+     */
     public Real(double n){
         this.realPart = n;
     }
 
+    /**
+     *
+     * @return Devulve la parte real del objeto
+     */
     public double toNumber(){
         return this.realPart;
     }
 
+    /**
+     * Convierte una gran cantidad de números a objetos del tipo Real
+     * @param numbers Lista o serie de números a transformar
+     * @return Lista de Reales equivalentes a lo dado
+     */
     public static Real[] toReal(double... numbers){
         Real []reals = new Real[numbers.length];
         for (int i = 0; i < numbers.length; i++){
